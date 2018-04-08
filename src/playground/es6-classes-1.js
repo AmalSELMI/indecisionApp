a056314203d097df4a6dd0ac7e5f834ec4b7eccd
AmalSELMI <amal.selmi89@gmail.com>
2018-04-08 20:45:21 +0200

    styling

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,75 +1,53 @@
-// Setup constructor to take name and age(default to 0)
-// getDescription - Amaru is 28 years old.
 class Person {
-constructor(name = 'Defaulto', age = 0){
+  constructor(name = 'Anonymous', age = 0) {
     this.name = name;
     this.age = age;
-    // this allows the setting of the values passed in
- }
- getGreeting() {
-    //  return 'Hi ' + this.name + '!';
-    return `Hi ${this.name}!`
- }
-
-getDescription() {
-
-    return ` Hey, ${this.name} is ${this.age} years old.`
-}
+  }
+  getGreeting() {
+    return `Hi. I am ${this.name}!`;
+  }
+  getDescription() {
+    return `${this.name} is ${this.age} year(s) old.`;
+  }
 }
 
-class Student extends Person { 
-    constructor(name, age, major) {
-     super(name, age);
-    // super: calls the parent element > Person.constructor >>name&age setting
-      this.major = major;
-}
-    hasMajor() {
-        return !!this.major;
-     // flipping twice with ! returns the boolean value
-}
-    getDescription(){
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDescription() {
+    let description = super.getDescription();
 
-        let description = super.getDescription();
-
-        if(this.hasMajor()) {
-            description += `Their major is ${this.major}.`
-        }
-        
-    return description
+    if (this.hasMajor()) {
+      description += ` Their major is ${this.major}.`;
     }
+
+    return description;
+  }
 }
-
-const me = new Student('Amaru', 28,'accounting');
-console.log(me.getGreeting());
-// // console.log(me.getDescription());
-
-// let other= new Student();
-// console.log(other.getGreeting());
-// console.log(other.getDescription());
-// console.log(other.getDescription());
-// console.log(other.getGreeting())
-
-
-// Challenge: create sub class: Traveler -> Person
-// Add support for homeLocation (3rd arg like major for student)
-// Override getGreeting
-//I am Amaru, i'm visiting from Paris || I am Amaru
 
 class Traveler extends Person {
-    constructor(name,age,homeLocation) {
-        super(name, age);
-        this.homeLocation = homeLocation;
-    }
-    getGreeting() {
-        let greeting = super.getGreeting();
-        if(this.homeLocation) {
-            greeting += `I'm visiting from ${this.homeLocation}.`;
-        }
-        return greeting;
-    }
-}
-const trav = new Traveler ('Nido', 26, 'Arizona');
-console.log(trav.getGreeting());
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+  getGreeting() {
+    let greeting = super.getGreeting();
 
-const other= new Traveler('Nico', 32, 'nowhere');
+    if (this.homeLocation) {
+      greeting += ` I am visiting from ${this.homeLocation}.`;
+    }
+
+    return greeting;
+  }
+}
+
+const me = new Traveler('Andrew Mead', 26, 'Philadelphia');
+console.log(me.getGreeting());
+
+const other = new Traveler(undefined, undefined, 'Nowhere');
 console.log(other.getGreeting());
